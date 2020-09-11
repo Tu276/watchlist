dc67419a2cf91565d9f1281e1cdd8300dea15b34
Tu276 <tu276nathan@gmail.com>
2020-09-11 17:08:34 +0300

    add initial movie.py file

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from .config import DevConfig
 
 # initializing app
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
 
 # setting up configuration
 app.config.from_object(DevConfig)
