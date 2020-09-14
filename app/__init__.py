791c58e2ad487ec7b80b8295ffa379c27c0322b5
Tu276 <tu276nathan@gmail.com>
2020-09-14 20:12:06 +0300

    add bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from .config import DevConfig
 
 # initializing app
@@ -8,4 +9,6 @@ app = Flask(__name__,instance_relative_config = True)
 app.config.from_object(DevConfig)
 app.config.from_pyfile("config.py")
 
+#initializing flask extensions
+bootstrap = Bootstrap(app)
 from app import views
